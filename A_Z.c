e34e990361ddb3cdc9f254b74ac1faa767d0eb25
riyaarora11 <68532163+riyaarora11@users.noreply.github.com>
2020-07-23 21:25:30 +0530

    Add files via upload

---
+++ A_Z.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i;
+    printf("The Alphabets are :\n");
+    for(i=65;i<=90;i++)
+     {
+        printf("%c\n",i);
+     }
+     getch();
+}
